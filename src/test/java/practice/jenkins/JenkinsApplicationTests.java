7c0877e02a5696c7c73110718389318af0c0ccc0
lkhlkh23 <lkhlkh09@gmail.com>
2024-07-01 21:51:48 +0900

    first commit

---
+++ src/test/java/practice/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
