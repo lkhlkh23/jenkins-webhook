05a2b00ed8fee373ab96dbed3488f6438028724d
lkhlkh23 <lkhlkh09@gmail.com>
2024-07-01 22:16:46 +0900

    add comment

--- src/main/java/practice/jenkins/JenkinsApplication.java
+++ src/main/java/practice/jenkins/JenkinsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsApplication {
 
 	public static void main(String[] args) {
+		// comment
 		SpringApplication.run(JenkinsApplication.class, args);
 	}
 
